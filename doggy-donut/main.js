9e99bc8e983730fc60d06c364205b9e09de8d7bc
lisaztruong <lisaztruong@gmail.com>
2017-04-16 16:31:20 -0700

    done game, must go back to redo comments

--- doggy-donut/main.js
+++ doggy-donut/main.js
@@ -1,56 +1,155 @@
-// contains game
+// Create our 'main' state that will contain the game
 var mainState = {
-
-  // execute in beginning to load images/ sound
   preload: function() {
-    // load dog sprite
-    game.load.image('dog', 'assets/bird.png');
+      // Load the bird sprite
+      game.load.image('bird', 'assets/bird.png');
+
+      game.load.image('pipe', 'assets/pipe.png');
+
+      game.load.audio('jump', 'assets/jump.wav');
+
+
   },
 
-  // next function where we set up game, display sprites, etc
   create: function() {
-    // change background color of game to grey
-    game.stage.backgroundColor = '#f1f2eb';
-    // set physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    // display dog at x=100, y=245
-    this.dog = game.add.sprite(100,245,'dog');
-    // add physics to dog so it can move (gravity, collision)
-    game.physics.arcade.enable(this.dog);
-    // add gravity so dog falls
-    this.dog.body.gravity.y = 1000;
-    // call jump function when spacebar is pressed
-    var spaceKey = game.input.keyboard.addKey(
-        Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);
+      // Change the background color of the game to blue
+      game.stage.backgroundColor = '#71c5cf';
+
+      // Set the physics system
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      // Display the bird at the position x=100 and y=245
+      this.bird = game.add.sprite(100, 245, 'bird');
+
+      // Add physics to the bird
+      // Needed for: movements, gravity, collisions, etc.
+      game.physics.arcade.enable(this.bird);
+
+      // Add gravity to the bird to make it fall
+      this.bird.body.gravity.y = 1000;
+
+      // Call the 'jump' function when the spacekey is hit
+      var spaceKey = game.input.keyboard.addKey(
+                      Phaser.Keyboard.SPACEBAR);
+      spaceKey.onDown.add(this.jump, this);
+
+      // Create an empty group
+      this.pipes = game.add.group();
+
+      this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+      this.score = 0;
+      this.labelScore = game.add.text(20, 20, "0",
+          { font: "30px Arial", fill: "#ffffff" });
+
+      // Move the anchor to the left and downward
+      this.bird.anchor.setTo(-0.2, 0.5);
+
+      this.jumpSound = game.add.audio('jump');
+
   },
 
-  // called periodically 60 times/s and contains game logic
   update: function() {
-    // if bird is out of screen (too high or low), restart game
-    if (this.dog.y < 0 || this.dog.y > 600)
-      this.restartGame();
+      // If the bird is out of the screen (too high or too low)
+      // Call the 'restartGame' function
+      if (this.bird.y < 0 || this.bird.y > 490){
+        this.restartGame();
+      }
+
+      game.physics.arcade.overlap(
+        this.bird, this.pipes, this.hitPipe, null, this);
+
+      if (this.bird.angle < 20){
+        this.bird.angle += 1;
+      }
+
   },
 
-  // dog jumps
+    // Make the bird jump
   jump: function() {
-    // add vertical velocityto bird
-    ths.dog.body.velocity.y = -350;
+      if (this.bird.alive == false){
+        return;
+      }
+
+      // Add a vertical velocity to the bird
+      this.bird.body.velocity.y = -350;
+
+      // Create an animation on the bird
+      var animation = game.add.tween(this.bird);
+
+      // Change the angle of the bird to -20° in 100 milliseconds
+      animation.to({angle: -20}, 100);
+
+      // And start the animation
+      animation.start();
+
+      this.jumpSound.play();
+
   },
 
-  // restart game
+  // Restart the game
   restartGame: function() {
-    // start with main state which restarts the game
-    game.State.start('main');
-  }
-  
+      // Start the 'main' state, which restarts the game
+      game.state.start('main');
+  },
+
+  addOnePipe: function(x, y) {
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
+
+    // Enable physics on the pipe
+    game.physics.arcade.enable(pipe);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200;
+
+    // Automatically kill the pipe when it's no longer visible
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function() {
+      // Randomly pick a number between 1 and 5
+      // This will be the hole position
+      var hole = Math.floor(Math.random() * 5) + 1;
+
+      // Add the 6 pipes
+      // With one big hole at position 'hole' and 'hole + 1'
+      for (var i = 0; i < 8; i++)
+          if (i != hole && i != hole + 1)
+              this.addOnePipe(400, i * 60 + 10);
+
+      this.score += 1;
+      this.labelScore.text = this.score;
+  },
+
+  hitPipe: function() {
+      // If the bird has already hit a pipe, do nothing
+      // It means the bird is already falling off the screen
+      if (this.bird.alive == false)
+          return;
+
+      // Set the alive property of the bird to false
+      this.bird.alive = false;
+
+      // Prevent new pipes from appearing
+      game.time.events.remove(this.timer);
+
+      // Go through all the pipes, and stop their movement
+      this.pipes.forEach(function(p){
+          p.body.velocity.x = 0;
+      }, this);
+  },
 };
 
-// initialize phaser... create 500px by 600px game
-var game = new Phaser.Game(500,600);
+// Initialize Phaser, and create a 400px by 490px game
+var game = new Phaser.Game(400, 490);
 
-// add mainState called lisa
-game.state.add('lisa', mainState);
+// Add the 'mainState' and call it 'main'
+game.state.add('main', mainState);
 
-// start state which starts the game
-game.state.start('lisa');
+// Start the state to actually start the game
+game.state.start('main');
