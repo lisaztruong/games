dd1abbc5a0119921451dd9a7bfc4fbbcf501928d
lisaztruong <lisaztruong@gmail.com>
2017-04-16 18:14:21 -0700

    change images

--- doggy-donut/main.js
+++ doggy-donut/main.js
@@ -4,10 +4,10 @@
 // Create our 'main' state that will contain the game
 var mainState = {
   preload: function() {
-      // Load the bird sprite
-      game.load.image('bird', 'assets/bird.png');
+      // Load the doggy sprite
+      game.load.image('doggy', 'assets/doggy.png');
 
-      game.load.image('pipe', 'assets/pipe.png');
+      game.load.image('donut', 'assets/donut.png');
 
       game.load.audio('jump', 'assets/jump.wav');
 
@@ -21,15 +21,15 @@ var mainState = {
       // Set the physics system
       game.physics.startSystem(Phaser.Physics.ARCADE);
 
-      // Display the bird at the position x=100 and y=245
-      this.bird = game.add.sprite(100, 245, 'bird');
+      // Display the doggy at the position x=100 and y=245
+      this.doggy = game.add.sprite(100, 245, 'doggy');
 
-      // Add physics to the bird
+      // Add physics to the doggy
       // Needed for: movements, gravity, collisions, etc.
-      game.physics.arcade.enable(this.bird);
+      game.physics.arcade.enable(this.doggy);
 
-      // Add gravity to the bird to make it fall
-      this.bird.body.gravity.y = 1000;
+      // Add gravity to the doggy to make it fall
+      this.doggy.body.gravity.y = 1000;
 
       // Call the 'jump' function when the spacekey is hit
       var spaceKey = game.input.keyboard.addKey(
@@ -37,7 +37,7 @@ var mainState = {
       spaceKey.onDown.add(this.jump, this);
 
       // Create an empty group
-      this.pipes = game.add.group();
+      this.donuts = game.add.group();
 
       this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 
@@ -46,41 +46,41 @@ var mainState = {
           { font: "30px Arial", fill: "#ffffff" });
 
       // Move the anchor to the left and downward
-      this.bird.anchor.setTo(-0.2, 0.5);
+      this.doggy.anchor.setTo(-0.2, 0.5);
 
       this.jumpSound = game.add.audio('jump');
 
   },
 
   update: function() {
-      // If the bird is out of the screen (too high or too low)
+      // If the doggy is out of the screen (too high or too low)
       // Call the 'restartGame' function
-      if (this.bird.y < 0 || this.bird.y > 490){
+      if (this.doggy.y < 0 || this.doggy.y > 490){
         this.restartGame();
       }
 
       game.physics.arcade.overlap(
-        this.bird, this.pipes, this.hitPipe, null, this);
+        this.doggy, this.donuts, this.hitPipe, null, this);
 
-      if (this.bird.angle < 20){
-        this.bird.angle += 1;
+      if (this.doggy.angle < 20){
+        this.doggy.angle += 1;
       }
 
   },
 
-    // Make the bird jump
+    // Make the doggy jump
   jump: function() {
-      if (this.bird.alive == false){
+      if (this.doggy.alive == false){
         return;
       }
 
-      // Add a vertical velocity to the bird
-      this.bird.body.velocity.y = -350;
+      // Add a vertical velocity to the doggy
+      this.doggy.body.velocity.y = -350;
 
-      // Create an animation on the bird
-      var animation = game.add.tween(this.bird);
+      // Create an animation on the doggy
+      var animation = game.add.tween(this.doggy);
 
-      // Change the angle of the bird to -20° in 100 milliseconds
+      // Change the angle of the doggy to -20° in 100 milliseconds
       animation.to({angle: -20}, 100);
 
       // And start the animation
@@ -97,21 +97,21 @@ var mainState = {
   },
 
   addOnePipe: function(x, y) {
-    // Create a pipe at the position x and y
-    var pipe = game.add.sprite(x, y, 'pipe');
+    // Create a donut at the position x and y
+    var donut = game.add.sprite(x, y, 'donut');
 
-    // Add the pipe to our previously created group
-    this.pipes.add(pipe);
+    // Add the donut to our previously created group
+    this.donuts.add(donut);
 
-    // Enable physics on the pipe
-    game.physics.arcade.enable(pipe);
+    // Enable physics on the donut
+    game.physics.arcade.enable(donut);
 
-    // Add velocity to the pipe to make it move left
-    pipe.body.velocity.x = -200;
+    // Add velocity to the donut to make it move left
+    donut.body.velocity.x = -200;
 
-    // Automatically kill the pipe when it's no longer visible
-    pipe.checkWorldBounds = true;
-    pipe.outOfBoundsKill = true;
+    // Automatically kill the donut when it's no longer visible
+    donut.checkWorldBounds = true;
+    donut.outOfBoundsKill = true;
   },
 
   addRowOfPipes: function() {
@@ -119,7 +119,7 @@ var mainState = {
       // This will be the hole position
       var hole = Math.floor(Math.random() * 5) + 1;
 
-      // Add the 6 pipes
+      // Add the 6 donuts
       // With one big hole at position 'hole' and 'hole + 1'
       for (var i = 0; i < 8; i++)
           if (i != hole && i != hole + 1)
@@ -130,19 +130,19 @@ var mainState = {
   },
 
   hitPipe: function() {
-      // If the bird has already hit a pipe, do nothing
-      // It means the bird is already falling off the screen
-      if (this.bird.alive == false)
+      // If the doggy has already hit a donut, do nothing
+      // It means the doggy is already falling off the screen
+      if (this.doggy.alive == false)
           return;
 
-      // Set the alive property of the bird to false
-      this.bird.alive = false;
+      // Set the alive property of the doggy to false
+      this.doggy.alive = false;
 
-      // Prevent new pipes from appearing
+      // Prevent new donuts from appearing
       game.time.events.remove(this.timer);
 
-      // Go through all the pipes, and stop their movement
-      this.pipes.forEach(function(p){
+      // Go through all the donuts, and stop their movement
+      this.donuts.forEach(function(p){
           p.body.velocity.x = 0;
       }, this);
   },
